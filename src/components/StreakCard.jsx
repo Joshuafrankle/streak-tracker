4f5fb5a607c10cea8525ed5ef4482f244497a48f
Joshuafrankle <joshuafrankle7@gmail.com>
2021-09-19 12:30:34 +0530

    UI changes

---
+++ src/components/StreakCard.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Button from "@mui/material/Button";
+
+export default function StreakCard() {
+  const [num, setNum] = useState(0);
+
+  function handleIncrement() {
+    setNum((prevNum) => prevNum + 1);
+  }
+  function handleDecrement() {
+    setNum((prevNum) => prevNum - 1);
+  }
+  function handleReset() {
+    setNum(0);
+  }
+  return (
+    <>
+      <div className="streak-card text-center">
+        <h1>Hello</h1>
+        <h1 className="mb-4">{num}</h1>
+        <div className="btn-section">
+          <Button variant="contained" onClick={handleReset}>
+            &#x21bb;
+          </Button>
+          <Button variant="contained" onClick={handleIncrement}>
+            +
+          </Button>
+          <Button variant="contained" onClick={handleDecrement}>
+            -
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import StreakCard from "../components/StreakCard";
+
 export default function LandingPage() {
+  const [streakCard, setStreakCard] = useState([]);
+  const [num, setNum] = useState(0);
+
+  function handleAdd() {
+    setStreakCard((prev) => [...prev, <StreakCard />]);
+  }
+
   return (
     <>
       <section className="landing-main">
-        <h1>Streak Tracker</h1>
+        <h1 className="streak-text">Streak Tracker</h1>
+      </section>
+
+      <section className="streak-cards mt-5">
+        {streakCard.length > 0 ? streakCard : <p>Nothing</p>}
+      </section>
+
+      <section className="longest-streak">
+        <h2 className="text-center">Streak Record</h2>
+        <div className="streak-record d-flex">
+          <h3>Smallville: {num}</h3>
+        </div>
+        <h2 className="text-center">Longest Streak {num} for something</h2>
+      </section>
+
+      <section className="">
+        <div className="ml-auto">
+          <Fab color="secondary" aria-label="add" onClick={handleAdd}>
+            <AddIcon />
+          </Fab>
+        </div>
       </section>
     </>
   );
