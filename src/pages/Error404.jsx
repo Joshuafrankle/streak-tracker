26667bbea17248a087b9754a1b9aeba68efdf0b8
Joshuafrankle <joshuafrankle7@gmail.com>
2021-09-19 09:55:26 +0530

    Initial commit

---
+++ src/pages/Error404.jsx
@@ -0,0 +1,9 @@
+export default function Error404() {
+  return (
+    <>
+      <section>
+        <h1>Error 404</h1>
+      </section>
+    </>
+  );
+}
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+export default function LandingPage() {
+  return (
+    <>
+      <section>
+        <h1>Hello World</h1>
+      </section>
+    </>
+  );
+}
