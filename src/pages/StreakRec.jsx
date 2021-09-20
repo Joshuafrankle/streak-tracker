3e72c22f9378f2c22fcdc91a7c5435d7a6875c85
Joshuafrankle <joshuafrankle7@gmail.com>
2021-09-21 00:09:28 +0530

    StreakRec

--- src/pages/StreakRec.jsx
+++ src/pages/StreakRec.jsx
@@ -1,16 +1,38 @@
-export default function StreakRec({ data, longStreak }) {
+import { useSelector } from "react-redux";
+import { Divider } from "@mui/material";
+
+export default function StreakRec() {
+  const streaks = useSelector((state) => state.streak);
+
+  const largestStreak = Math.max.apply(
+    Math,
+    streaks.map((stk) => stk.streakCount)
+  );
+
+  const largestStreakTitle = streaks.map((stk) =>
+    stk.streakCount === largestStreak ? stk.title : null
+  );
+
   return (
     <>
       <section className="longest-streak m-5">
         <div className="text-center">
           <h2>Streak Record</h2>
           <h3>
-            Longest Streak is {longStreak} for {longStreak}
+            Longest Streak is {largestStreak} for {largestStreakTitle}
           </h3>
         </div>
-        <div className="d-flex">
-          <h3 className="text-muted">Smallville: 0</h3>
+        <div className="d-flex record-list mt-5">
+          {streaks.map((streak) => (
+            <>
+              <h3 className="text-muted">
+                {streak.title}: {streak.streakCount}
+              </h3>
+              <p className="mt-1">&bull;</p>
+            </>
+          ))}
         </div>
+        <Divider />
       </section>
     </>
   );
