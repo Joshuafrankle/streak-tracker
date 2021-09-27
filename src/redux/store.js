4d5e3ce8e2aa2f7d0f69a687afb3ffde4441dd99
Joshuafrankle <joshuafrankle7@gmail.com>
2021-09-27 11:01:25 +0530

    Combine redux toolkit and redux saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import { watcherSaga } from "./sagas/rootSaga";
 import streakReducer from "./slices/streakSlice";
+import userReducer from "./slices/userSlice";
 
-export default configureStore({
-  reducer: {
-    streak: streakReducer,
-  },
+const sagaMiddleware = createSagaMiddleware();
+
+const reducer = combineReducers({
+  streak: streakReducer,
+  user: userReducer,
 });
+
+const store = configureStore({
+  reducer,
+  middleware: [sagaMiddleware],
+});
+
+sagaMiddleware.run(watcherSaga);
+
+export default store;
