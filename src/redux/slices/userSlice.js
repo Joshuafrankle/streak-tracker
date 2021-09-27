940d0ecaa2a9a2a6eaccb28787924109d0426d20
Joshuafrankle <joshuafrankle7@gmail.com>
2021-09-27 11:17:22 +0530

    removed comments

--- src/redux/slices/userSlice.js
+++ src/redux/slices/userSlice.js
@@ -7,14 +7,10 @@ const userSlice = createSlice({
     getUser: (state, action) => {},
     setUser: (state, action) => {
       const userData = action.payload;
-      const abc = userData;
-      console.log("hello", abc);
       return { ...state, ...userData };
     },
   },
 });
-
-console.log("userSlice", userSlice);
 
 export const { getUser, setUser } = userSlice.actions;
 export default userSlice.reducer;
